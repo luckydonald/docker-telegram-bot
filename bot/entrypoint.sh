9c8a3eacea1b19468b291919f2d7945ef441348e
luckydonald <luckydonald@flutterb.at>
2017-11-21 00:01:37 +0100

    fixed travis (12)

--- bot/entrypoint.sh
+++ bot/entrypoint.sh
@@ -16,9 +16,28 @@ function cleanup {
         echo "socket gone"
     fi
 }
-trap cleanup EXIT;
 
-chown -R $USER_UID:$GROUP_UID /sockets/bots/;
-echo "exec gosu $USER_UID:$GROUP_UID $CMD $ARGS";
-set -ex;
-exec gosu $USER_UID:$GROUP_UID $CMD $ARGS;
+# this if will check if the first argument is a flag
+# but only works if all arguments require a hyphenated flag
+# -v; -SL; -f arg; etc will work, but not arg1 arg2
+if [ "${1:0:1}" = '-' ]; then
+    set -- /usr/local/bin/uwsgi "$@"
+elif [ -n "${1}" ]; then
+    # empty argument call
+    set -- /usr/local/bin/uwsgi
+elif [ "${1}" = 'uwsgi' ]; then
+    # just uwsgi
+    set -- /usr/local/bin/uwsgi
+fi
+if [ "$1" == '/usr/local/bin/uwsgi' ]; then
+    # the expected command
+    trap cleanup EXIT;
+
+    chown -R $USER_UID:$GROUP_UID /sockets/bots/;
+    echo "exec gosu $USER_UID:$GROUP_UID $CMD $ARGS";
+    set -ex;
+    exec gosu $USER_UID:$GROUP_UID $CMD $ARGS;
+else
+    # else default to run whatever the user wanted like "bash"
+    exec "$@"
+fi
